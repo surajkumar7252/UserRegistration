24ed1e17b5813f087afa5480dc5216bc279b07e2
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-02 07:41:47 +0530

    [Surajmal Kumar]. Commiting welcome page

---
+++ EmailValidatorProgram.java
@@ -0,0 +1,22 @@
+//SURAJMAL KUMAR
+
+//User Registration Program
+
+//Welcome 
+
+
+
+
+
+
+public class EmailValidatorProgram {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration Program. ");
+		
+}
+	
+}
+
