dd9f370b88be19cfa61806f7ad592db2188b3ca6
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-02 07:44:27 +0530

    [Surajmal Kumar]. Checking First Name. Code success.

--- EmailValidatorProgram.java
+++ EmailValidatorProgram.java
@@ -2,20 +2,48 @@
 
 //User Registration Program
 
-//Welcome 
+//Use case 1 i.e. Checking for the correct First Name.
+
+//https://github.com/surajkumar7252/UserRegistration.git
 
 
 
-
+import java.util.Scanner;
+import java.util.regex.*;
 
 
 public class EmailValidatorProgram {
 	
+	private Pattern firstNamePattern;
+	private Matcher matchPattern;
 	
+	static Scanner sc= new Scanner(System.in);
+	
+	private static final String CHECK_FIRST_NAME_TEMPLATE ="^[A-Za-z0-9]{3,25}$";
+	
+	public EmailValidatorProgram(){
+		
+		firstNamePattern=Pattern.compile(CHECK_FIRST_NAME_TEMPLATE);
+		}
+
+	public boolean check( String firstName)
+	{
+		matchPattern=firstNamePattern.matcher(firstName);
+		return matchPattern.matches();
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to the User Registration Program. ");
-		
+		System.out.println("Enter the First Name: ");
+		String lastName=sc.nextLine();
+		EmailValidatorProgram firstNameCheck = new EmailValidatorProgram();
+		boolean value=firstNameCheck.check(lastName);
+		if(value) {
+			System.out.println("First Name is Correct.");
+		}
+		else if(!value) {
+			System.out.println("You entered a Wrong First Name.");
+		}
+			
 }
 	
 }
