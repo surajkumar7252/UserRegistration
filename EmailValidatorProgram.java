2efd5602e5f9deb12d58445245e3e187f662317a
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-05 07:24:36 +0530

    [Surajmal Kumar] Adding Phone number checker success'

--- EmailValidatorProgram.java
+++ EmailValidatorProgram.java
@@ -4,7 +4,7 @@
 
 //User Registration Program
 
-//Use case 2 i.e. Checking for the correct email id..
+//Use case 2 i.e. Checking for the correct Phone Number..
 
 //https://github.com/surajkumar7252/UserRegistration.git
 
@@ -19,24 +19,30 @@ public class EmailValidatorProgram {
 	private Pattern lastNamePattern;
 	private Pattern firstNamePattern;
 	private Pattern emailPattern;
+	private Pattern phoneNumberPattern;
 	private Matcher matchPatternLastName;
 	private Matcher matchPatternFirstName;
 	private Matcher matchPatternEmail;
+	private Matcher matchPatternPhone;
 	
 	static Scanner lastnamefeed= new Scanner(System.in);
 	static Scanner firstnamefeed= new Scanner(System.in);
 	static Scanner emailFeed= new Scanner(System.in);
+	static Scanner phoneFeed= new Scanner(System.in);
 	
 	
 	
     private static final String CHECK_NAME_TEMPLATE ="^[A-Za-z0-9]{3,25}$";
     private static final String CHECK_EMAIL_TEMPLATE ="^[a-zA-Z0-9]+([_+-.]{1}[a-zA-Z0-9]+)?@[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,}([_+-.]{1}[a-zA-Z]{2,})?";
+    private static final String CHECK_PHONE_NUMBER_TEMPLATE ="^[1-9]{1,3}[ ]{1}[1-9]{1}[0-9]{9}";
     
    public EmailValidatorProgram(){
-		
+	
 		firstNamePattern=Pattern.compile(CHECK_NAME_TEMPLATE);
 		lastNamePattern=Pattern.compile(CHECK_NAME_TEMPLATE);
 		emailPattern=Pattern.compile(CHECK_EMAIL_TEMPLATE);
+		phoneNumberPattern=Pattern.compile(CHECK_PHONE_NUMBER_TEMPLATE);
+		
 		}
 	
 	
@@ -48,14 +54,20 @@ public class EmailValidatorProgram {
 	
 	public boolean checkFirstName( String firstName)
 	{
-		matchPatternFirstName=lastNamePattern.matcher(firstName);
+		matchPatternFirstName=firstNamePattern.matcher(firstName);
 		return matchPatternFirstName.matches();
 	}
 	
 	public boolean checkEmail( String email)
 	{
 		matchPatternEmail=emailPattern.matcher(email);
-		return matchPatternLastName.matches();
+		return matchPatternEmail.matches();
+	}
+	
+	public boolean checkPhoneNumber( String phone)
+	{
+		matchPatternPhone=phoneNumberPattern.matcher(phone);
+		return matchPatternPhone.matches();
 	}
 	public static void main(String[] args) {
 		System.out.println("Enter the First Name: ");
@@ -67,13 +79,18 @@ public class EmailValidatorProgram {
 		System.out.println("Enter the Email-Id: ");
 		String email=emailFeed.nextLine();
 		
+		System.out.println("Enter the Email-Id: ");
+		String phone=phoneFeed.nextLine();
+		
 		EmailValidatorProgram lastNameCheck = new EmailValidatorProgram();
 		EmailValidatorProgram firstNameCheck = new EmailValidatorProgram();
 		EmailValidatorProgram emailCheck = new EmailValidatorProgram();
+		EmailValidatorProgram phoneCheck = new EmailValidatorProgram();
 		
 		boolean firstResponse=firstNameCheck.checkLastName(firstName);
 		boolean lastResponse=lastNameCheck.checkLastName(lastName);
-		boolean emailResponse=emailCheck.checkLastName(email);
+		boolean emailResponse=emailCheck.checkEmail(email);
+		boolean phoneResponse=phoneCheck.checkPhoneNumber(phone);
 		
 		if(firstResponse) {
 			System.out.println("First Name is Correct.");
@@ -93,6 +110,13 @@ public class EmailValidatorProgram {
 		}
 		else if(!emailResponse) {
 			System.out.println("You entered a Wrong Email ID.");
+		}
+		
+		if(phoneResponse) {
+			System.out.println("Phone Number is Correct.");
+		}
+		else if(!phoneResponse) {
+			System.out.println("You entered a Wrong Phone Number.");
 		}
 			
 }
